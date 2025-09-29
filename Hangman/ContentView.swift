1d1d3d2e73d7272bc3d2d908d1515507165b26f0
Colin O'Connor <ColinOC99@gmail.com>
2025-09-29 12:23:52 -0400

    Initial Commit

---
+++ Hangman/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hangman
+//
+//  Created by Colin O'Connor on 9/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hangman/HangmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangmanApp.swift
+//  Hangman
+//
+//  Created by Colin O'Connor on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct HangmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
